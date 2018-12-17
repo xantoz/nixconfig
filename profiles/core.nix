c93228228a4dc4cdb1d43db4e96d88641849d697
Anton Kindestam <antonki@kth.se>
2018-12-17 21:36:55 +0100

    core: don't be in the input group

--- profiles/core.nix
+++ profiles/core.nix
@@ -29,6 +29,6 @@
   users.users.tewi_inaba = {
     isNormalUser = true;
     uid = 1000;
-    extraGroups = [ "wheel" "systemd-journal" "audio" "video" "render" "dialout" "lp" "input" "cdrom" "floppy" ];
+    extraGroups = [ "wheel" "systemd-journal" "audio" "video" "render" "dialout" "lp" "cdrom" "floppy" ];
   };
 }
