b83b55194f4eb71dd06bd00b33b7883b400116ed
Anton Kindestam <antonki@kth.se>
2018-12-30 14:20:00 +0100

    laptop: add version of xbattbar patched for hp

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs;
+  let
+    my_xbattbar = haskellPackages.xbattbar.overrideAttrs(old: { patches = [ ../patches/xbattbar-0.2.patch ]; } );
+  in [
     powertop
+    my_xbattbar
   ];
 }
