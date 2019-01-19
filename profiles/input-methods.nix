6f72abf7ee0a164e111d70cd309470685ca45656
Anton Kindestam <antonki@kth.se>
2019-01-19 21:22:47 +0100

    profiles/input-methods: pointless change

--- profiles/input-methods.nix
+++ profiles/input-methods.nix
@@ -4,6 +4,6 @@
   i18n.inputMethod.enabled = "fcitx";
   i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ mozc m17n ];
 
-  services.xserver.layout = "us";
+  services.xserver.layout = "se(us)";
   services.xserver.xkbOptions = "ctrl:nocaps";
 }
