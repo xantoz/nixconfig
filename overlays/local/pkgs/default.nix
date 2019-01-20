4af2706972d60f6dfe7af2a04eb1d751b8ecf1db
Anton Kindestam <antonki@kth.se>
2019-01-20 01:46:52 +0100

    overlay/local: poke at python37Packages.dateparser override and wish
    Still doesn't seem to quite do what I want... oh well...

--- overlays/local/pkgs/default.nix
+++ overlays/local/pkgs/default.nix
@@ -2,11 +2,11 @@ self: super:
 
 {
   # back-port of https://github.com/ivan/nixpkgs/commit/b8f9ca39444398e1078bf25859c62f46af18925e
-  python37Packages.dateparser = super.python37Packages.dateparser.overrideAttrs(old: {
+  python37Packages.dateparser = super.python37Packages.dateparser.overridePythonAttrs(old: {
       preCheck =''
         # skip because of missing convertdate module, which is an extra requirement
         rm tests/test_jalali.py
-g
+
         # skip because of failures with Python 3.7: https://github.com/NixOS/nixpkgs/issues/52766
         rm tests/test_search.py
       '';
