4a3812f4fa5c9ca62f62f0cbca12712d23c1ec83
Anton Kindestam <antonki@kth.se>
2019-02-16 04:03:26 +0100

    update dotfiles submodule, and link in .zile and .config/feh/themes from it

--- home-xantoz-nixpkgs/home.nazrin.nix
+++ home-xantoz-nixpkgs/home.nazrin.nix
@@ -24,7 +24,9 @@
 
   home.file = {
     ".screenrc".source = builtins.path { name = "dotscreenrc"; path = ./config/dotfiles/src/.screenrc; };
+    ".zile".source = builtins.path { name = "dotzile"; path = ./config/dotfiles/src/.zile; };
     ".config/youtube-dl/config".source = ./config/dotfiles/src/youtube-dl/config;
+    ".config/feh/themes".source = ./config/dotfiles/src/feh/themes;
 
     ".config/ratpoison".source = ./config/ratpoison;
     ".ratpoisonrc".source = pkgs.writeText "dotratpoisonrc" ''
