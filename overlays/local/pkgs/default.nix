6da1eaf753a1a56eaae511085ce74a8a1f5fa35c
Anton Kindestam <antonki@kth.se>
2019-02-19 15:18:34 +0100

    overlays/local: add up to date versions of sigrok & pulseview to overlay
    TODO: fetch from the actual upstream git repos, and not the github mirror

--- overlays/local/pkgs/default.nix
+++ overlays/local/pkgs/default.nix
@@ -20,5 +20,45 @@ with super.lib; {
       super.pkgs.mesa_noglu
     ];
   });
+
+  libsigrokdecode = super.libsigrokdecode.overrideAttrs(old: {
+    src = super.fetchFromGitHub {
+      repo = "libsigrokdecode";
+      owner = "sigrokproject";
+      rev = "49d3e6f83c68e62138621f94bf5e22c9d7717fca";
+      sha256 = "07gjg881gj9718hmc0815jvspzs2dx7d05jksk2ixlslqyjdphc2";
+    };
+    version = "git";
+    name = "libsigrokdecode-git";
+    nativeBuildInputs = old.nativeBuildInputs ++ [ super.autoreconfHook ];
+  });
+
+  libsigrok = super.libsigrok.overrideAttrs(old: {
+    src = super.fetchFromGitHub {
+      repo = "libsigrok";
+      owner = "sigrokproject";
+      rev = "1b6b9c01dfd33a75b22d7b5e0d6a284ae37b39e1";
+      sha256 = "08i6bkvjm7c7176idzdl83ckxmpzdfiz3issji01y3myscyfpwjr";
+    };
+    version = "git";
+    name = "libsigrok-git";
+    nativeBuildInputs = old.nativeBuildInputs ++ [ super.autoreconfHook ];
+    firmware = super.fetchurl {
+      url = "https://sigrok.org/download/binary/sigrok-firmware-fx2lafw/sigrok-firmware-fx2lafw-bin-0.1.6.tar.gz";
+      sha256 = "14sd8xqph4kb109g073daiavpadb20fcz7ch1ipn0waz7nlly4sw";
+    };
+  });
+
+  pulseview = super.pulseview.overrideAttrs(old: {
+    src = super.fetchFromGitHub {
+      repo = "pulseview";
+      owner = "sigrokproject";
+      rev = "baef263a884ee4ff4d3dd15cd4d334fa9178b090";
+      sha256 = "0fxpisrjk00ird0qypb36cid198mc6y8k9grwcilgrpy4m8isn0z";
+    };
+    version = "git";
+    name = "pulseview-git";
+  });
+
   webmacs = super.callPackage ./webmacs { };
 }
