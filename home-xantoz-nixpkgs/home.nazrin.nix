179effc117c6fb2370e4c64702d3bc9372f86392
Anton Kindestam <antonki@kth.se>
2019-02-21 02:48:57 +0100

    home/config/dotfiles: update submodule, and install the redshift.conf

--- home-xantoz-nixpkgs/home.nazrin.nix
+++ home-xantoz-nixpkgs/home.nazrin.nix
@@ -27,6 +27,7 @@
     ".zile".source = builtins.path { name = "dotzile"; path = ./config/dotfiles/src/.zile; };
     ".config/youtube-dl/config".source = ./config/dotfiles/src/youtube-dl/config;
     ".config/feh/themes".source = ./config/dotfiles/src/feh/themes;
+    ".config/redshift.conf".source = ./config/dotfiles/src/redshift.conf;
 
     ".config/ratpoison".source = ./config/ratpoison;
     ".ratpoisonrc".source = pkgs.writeText "dotratpoisonrc" ''
