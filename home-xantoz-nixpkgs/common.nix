3a48a7c1364347fc5c5a69bfcfc3df7bfb837b37
Anton Kindestam <antonki@kth.se>
2019-02-23 14:38:17 +0100

    home/common: import my overlay in home-manager as well
    seems like I need to import the overlay separately for the
    home-manager context.

--- home-xantoz-nixpkgs/common.nix
+++ home-xantoz-nixpkgs/common.nix
@@ -3,6 +3,10 @@
 {
   nixpkgs.config.allowUnfree = true;
 
+  nixpkgs.overlays = [
+    (import ../overlays/local/pkgs/default.nix)
+  ];
+
   home.file = {
     ".screenrc".source = builtins.path { name = "dotscreenrc"; path = ./config/dotfiles/src/.screenrc; };
     ".zile".source = builtins.path { name = "dotzile"; path = ./config/dotfiles/src/.zile; };
