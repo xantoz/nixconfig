ea367b0517bdeb369b1ef4fca1fc77d1862d03cd
Anton Kindestam <antonki@kth.se>
2019-03-11 19:33:41 +0100

    home/emacs: start using use-package-name-extract

--- home/emacs.nix
+++ home/emacs.nix
@@ -8,18 +8,14 @@
     '')
   ];
 
-  programs.emacs.package = pkgs.emacs26;
-  programs.emacs.enable = true;
-  programs.emacs.extraPackages = epkgs: with epkgs; [
-    magit
-    ag
-    buffer-move
-    use-package
-    find-file-in-project
-    # lua-mode
-  ];
-
   services.emacs.enable = true;
+  programs.emacs.enable = true;
+  programs.emacs.package =
+    let
+      inherit (pkgs.callPackage ./config/emacs/scripts/nix-emacs-with-use-package-pkgs/emacs-with-use-package-pkgs.nix {}) emacsWithUsePackagePkgs;
+    in (emacsWithUsePackagePkgs {
+      config = ./config/emacs/init.el;
+    });
 
   home.file = {
     ".config/emacs".source = ./config/emacs;
