e5408462418f20b639fe24aceb9a68b5d0110fd5
Anton Kindestam <antonki@kth.se>
2019-03-12 06:38:04 +0100

    home/emacs: use the the new emacsUsePackagePkgsClosure function

--- home/emacs.nix
+++ home/emacs.nix
@@ -13,11 +13,8 @@
   programs.emacs.package = pkgs.emacs26;
   programs.emacs.extraPackages =
     let
-      packages =
-        let
-          inherit (pkgs.callPackage ./config/emacs/scripts/nix-emacs-with-use-package-pkgs/emacs-with-use-package-pkgs.nix {}) parsePackages;
-        in (parsePackages ./config/emacs/init.el);
-    in (epkgs: map (name: epkgs.${name}) (packages ++ [ "use-package" ]));
+      inherit (pkgs.callPackage ./config/emacs/scripts/nix-emacs-with-use-package-pkgs/emacs-with-use-package-pkgs.nix {}) emacsUsePackagePkgsClosure;
+    in emacsUsePackagePkgsClosure { config = ./config/emacs/init.el; };
 
   home.file = {
     ".config/emacs".source = ./config/emacs;
