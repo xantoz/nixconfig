fee5686cc3aafa6e16bdc9fdfda6cc0aa33c5dd5
Anton Kindestam <antonki@kth.se>
2019-04-08 23:35:24 +0200

    home/emacs: experiments in dumping emacs for really quick startup

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,13 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
-  home.packages = with pkgs; [
-    cscope
-    (writeShellScriptBin "ec" ''
-      exec emacsclient "$@"
-    '')
-  ];
-
   services.emacs.enable = true;
   programs.emacs.enable = true;
   programs.emacs.package = pkgs.emacsVcs27;
@@ -15,6 +8,23 @@
     let
       inherit (pkgs.callPackage ./config/emacs/scripts/nix-emacs-with-use-package-pkgs/emacs-with-use-package-pkgs.nix {}) usePackagePkgs;
     in usePackagePkgs { config = ./config/emacs/init.el; };
+
+  home.packages = with pkgs; [
+    cscope
+    (writeShellScriptBin "ec" ''
+      exec emacsclient "$@"
+    '')
+    (pkgs.runCommand "dumped-emacs" {} ''
+      mkdir -p $out/bin
+      mkdir -p $out/lib
+      HOME=$out ${config.programs.emacs.finalPackage}/bin/emacs -Q -nw --batch -l ${./config/emacs}/init.el --execute '(progn (write-region (format "(setq load-path (quote %S))" load-path) nil "'$out'/lib/emacs-loadpath.el") (dump-emacs-portable "'$out'/lib/emacs.bdump") (kill-emacs))'
+      cat > $out/bin/qemacs <<EOF
+      #!/bin/sh
+      exec ${config.programs.emacs.finalPackage}/bin/emacs -Q --dump-file $out/lib/emacs.bdump --load $out/lib/emacs-loadpath.el "\$@"
+      EOF
+      chmod +x $out/bin/qemacs
+    '')
+  ];
 
   home.file = {
     ".config/emacs".source = ./config/emacs;
