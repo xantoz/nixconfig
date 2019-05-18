e9980e9b1e8119789d7b1c3e2f4ed03cce7de725
Anton Kindestam <antonki@kth.se>
2019-05-18 09:24:29 +0200

    libplacebo: fix typo in license metadata

--- overlays/local/pkgs/libplacebo/default.nix
+++ overlays/local/pkgs/libplacebo/default.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
   meta = with stdenv.lib; {
     description = "libplacebo";
     homepage =  https://github.com/haasn/libplacebo;
-    license = licenses.lgpl21plus;
+    license = licenses.lgpl21Plus;
     platforms = platforms.linux;
   };
 }
