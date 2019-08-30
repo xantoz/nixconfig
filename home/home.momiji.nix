0336771def02c30f6d8b9b044528b6ac0ba7830b
Anton Kindestam <antonki@kth.se>
2019-08-30 20:41:00 +0200

    home/home.momiji: add wallpaper

--- home/home.momiji.nix
+++ home/home.momiji.nix
@@ -59,6 +59,8 @@
       xinput set-prop 'PS/2 Generic Mouse' 'libinput Middle Emulation Enabled' 1
       xinput set-prop 'PS/2 Generic Mouse' 'Coordinate Transformation Matrix' 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.300000
 
+      feh --bg-fill ${./wallpapers/Touhou.full.1536689.jpg} &
+
       export QT_IM_MODULE=fcitx
       export GTK_IM_MODULE=fcitx
       export CLUTTER_IM_MODULE=fcitx
