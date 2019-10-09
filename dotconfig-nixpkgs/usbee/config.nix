63dc7f0dc3467699ad07429834feb7e0ba40831e
Anton Kindestam <antonki@kth.se>
2019-10-09 09:16:16 +0200

    .config/nixpkgs usbee: use more sophisticated wrapper for most programs
    Using symlinkJoin and pkgs.makeWrapper
    
    Still need to fix the pulseview/nixIntelPythonWrap wrapper up.
    
    Also need to refactor the wrapper for the terminals (they need to
    undefine some environment variables in the shell running in it)

--- dotconfig-nixpkgs/usbee/config.nix
+++ dotconfig-nixpkgs/usbee/config.nix
@@ -1,5 +1,70 @@
 { pkgs, ...}:
 
+let
+  /*
+  ** Wraps binary for GL, Vulkan and VAAPI paths suitable for intel iGPUs.
+  ** Probably adds slightly too much to LD_LIBRARY_PATH, but whatever
+  */
+  nixIntelWrap = package: name: binaries:
+    pkgs.symlinkJoin {
+        name = name;
+        paths = [ package ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          for binary in ${pkgs.lib.concatStringsSep " " binaries}; do
+              wrapProgram "$out$binary"                                     \
+                --set LIBGL_DRIVERS_PATH "${pkgs.mesa_drivers}/lib/dri"     \
+                --set LIBVA_DRIVERS_PATH '${pkgs.vaapiIntel}/lib/dri'       \
+                --prefix LD_LIBRARY_PATH ":" ${pkgs.lib.makeLibraryPath [
+                    pkgs.mesa_drivers
+                    pkgs.zlib
+                    pkgs.libdrm
+                    pkgs.xorg.libX11
+                    pkgs.xorg.libxcb
+                    pkgs.xorg.libxshmfence
+                    pkgs.wayland
+                    pkgs.gcc.cc
+                    pkgs.expat
+                    pkgs.llvm_7
+                    pkgs.vulkan-loader
+                  ]}
+          done
+        '';
+    };
+
+  /*
+  ** Same as above, but also wraps python
+  */
+  nixIntelPythonWrap = package: name: binaries:
+    pkgs.symlinkJoin {
+        name = name;
+        paths = [ package ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          for binary in ${pkgs.lib.concatStringsSep " " binaries}; do
+              wrapProgram "$out$binary"                                     \
+                --set PYTHONNOUSERSITE 1                                    \
+                --set PYTHONHASHSEED 0                                      \
+                --prefix PYTHONPATH : "${pkgs.python3}/lib/${pkgs.python3.libPrefix}/site-packages" \
+                --set LIBGL_DRIVERS_PATH "${pkgs.mesa_drivers}/lib/dri"     \
+                --set LIBVA_DRIVERS_PATH '${pkgs.vaapiIntel}/lib/dri'       \
+                --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [
+                    pkgs.mesa_drivers
+                    pkgs.zlib
+                    pkgs.libdrm
+                    pkgs.xorg.libX11
+                    pkgs.xorg.libxcb
+                    pkgs.xorg.libxshmfence
+                    pkgs.wayland
+                    pkgs.gcc.cc
+                    pkgs.expat
+                    pkgs.llvm_7
+                    pkgs.vulkan-loader
+                  ]}
+          done
+        '';
+    };
+in
 {
   allowUnfree = true;
 
@@ -7,24 +72,7 @@
 
   packageOverrides = pkgs_: {
 
-    my_mpv = pkgs.writeShellScriptBin "mpv" ''
-      export LIBGL_DRIVERS_PATH='${pkgs.mesa_drivers}/lib/dri'
-      export LD_LIBRARY_PATH='${pkgs.lib.makeLibraryPath [
-        pkgs.mesa_drivers
-        pkgs.zlib
-        pkgs.libdrm
-        pkgs.xorg.libX11
-        pkgs.xorg.libxcb
-        pkgs.xorg.libxshmfence
-        pkgs.wayland
-        pkgs.gcc.cc
-        pkgs.expat
-        pkgs.llvm_7
-        pkgs.vulkan-loader
-      ]}:'$LD_LIBRARY_PATH
-      export LIBVA_DRIVERS_PATH='${pkgs.vaapiIntel}/lib/dri'
-      exec ${pkgs.mpv}/bin/mpv "$@"
-    '';
+    my_mpv = nixIntelWrap pkgs.mpv "mpv" [ "/bin/mpv" "/bin/umpv" ];
 
     my_alacritty = pkgs.writeShellScriptBin "alacritty" ''
       export LIBGL_DRIVERS_PATH='${pkgs.mesa_drivers}/lib/dri'
@@ -64,22 +112,11 @@
       exec ${pkgs.kitty}/bin/kitty "$@" -e /bin/sh -c "unset LIBGL_DRIVERS_PATH; unset LD_LIBRARY_PATH; unset LIBVA_DRIVERS_PATH; exec $SHELL -l"
     '';
 
-    my_mpc-qt = pkgs.writeShellScriptBin "mpc-qt" ''
-      export LIBGL_DRIVERS_PATH='${pkgs.mesa_drivers}/lib/dri'
-      export LD_LIBRARY_PATH='${pkgs.mesa_drivers}/lib':$LD_LIBRARY_PATH
-      exec ${pkgs.mpc-qt}/bin/mpc-qt "$@"
-    '';
+    my_mpc-qt = nixIntelWrap pkgs.mpc-qt "mpc-qt" [ "/bin/mpc-qt" ];
 
-    my_webmacs = pkgs.writeShellScriptBin "webmacs" ''
-      export LIBGL_DRIVERS_PATH='${pkgs.mesa_drivers}/lib/dri'
-      export LD_LIBRARY_PATH='${pkgs.mesa_drivers}/lib':$LD_LIBRARY_PATH
-      exec ${pkgs.webmacs}/bin/webmacs "$@"
-    '';
+    my_webmacs = nixIntelWrap pkgs.webmacs "webmacs" [ "/bin/webmacs" ];
 
-    my_vainfo = pkgs.writeShellScriptBin "vainfo" ''
-      export LIBVA_DRIVERS_PATH='${pkgs.vaapiIntel}/lib/dri'
-      exec ${pkgs.libva-utils}/bin/vainfo "$@"
-    '';
+    my_vainfo = nixIntelWrap pkgs.vainfo "vainfo" [ "/bin/vainfo" ];
 
     my_pulseview = pkgs.writeScriptBin "pulseview" ''
       #!/usr/bin/env nix-shell
@@ -89,6 +126,10 @@
       export LD_LIBRARY_PATH='${pkgs.mesa_drivers}/lib'""''${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH
       exec ${pkgs.pulseview}/bin/pulseview "$@"
     '';
+
+    /*
+    my_pulseview = nixIntelPythonWrap pkgs.pulseview "pulseview" [ "/bin/pulseview" ];
+    */
 
     nixGLIntel = pkgs.writeShellScriptBin "nixGLIntel" ''
       export LIBGL_DRIVERS_PATH='${pkgs.mesa_drivers}/lib/dri'
@@ -120,7 +161,7 @@
       paths = [
         pkgs.my_mpc-qt
         pkgs.my_mpv
-        # pkgs.my_webmacs
+        #pkgs.my_webmacs
 
         pkgs.emacs26
 
