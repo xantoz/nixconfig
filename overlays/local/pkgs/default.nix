abc520b1ab88bfbec48a5472ae362e7d83957285
Anton Kindestam <antonki@kth.se>
2019-11-05 18:41:40 +0100

    overlays/local/pkgs: bump mpv to personal-build--v43

--- overlays/local/pkgs/default.nix
+++ overlays/local/pkgs/default.nix
@@ -73,8 +73,8 @@ with super.lib; {
           libaom = custom_libaom;
           nvenc = false;
         });
-      mpv_rev = "ae407030016fbd8128b5ac092670a290c0e9f12c";
-      mpv_sha256 = "04rjwyald77p7yg0hl6il7wcqsz0x1dxip71nc4sk303zi9yg39w";
+      mpv_rev = "f527d7b11c31de0f73956cc4eaf92efe637b8809";
+      mpv_sha256 = "1g1qn2ikxh4ns82bbifsp79la9n6vz4x7dyr2xyns4bfn9bdb7j1";
       fakegit = super.writeShellScriptBin "git" ''
         echo "${mpv_rev}"
       '';
