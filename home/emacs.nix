400597d3c1c78eea56feaa69e7156a9ba04a9e57
Anton Kindestam <antonki@kth.se>
2019-11-25 08:42:51 +0100

    home/emacs: add vterm to emacs packages
    this is not marked :ensure in init.el, so we add it manually here

--- home/emacs.nix
+++ home/emacs.nix
@@ -6,8 +6,13 @@
   programs.emacs.package = pkgs.emacsVcs27;
   programs.emacs.extraPackages =
     let
-      inherit (pkgs.callPackage ./config/emacs/scripts/nix-emacs-with-use-package-pkgs/emacs-with-use-package-pkgs.nix { emacs = pkgs.emacsVcs27; }) usePackagePkgs;
-    in usePackagePkgs { config = ./config/emacs/init.el; };
+      inherit (pkgs.callPackage ./config/emacs/scripts/nix-emacs-with-use-package-pkgs/emacs-with-use-package-pkgs.nix {
+        emacs = pkgs.emacsVcs27;
+      }) usePackagePkgs;
+    in usePackagePkgs {
+      config = ./config/emacs/init.el;
+      extraPackages = [ "vterm" ];
+    };
 
   home.packages = with pkgs; [
     cscope
