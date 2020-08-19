82354b22306dffb8400c6fab549686686a02e9b2
Anton Kindestam <antonki@kth.se>
2020-08-19 12:33:34 +0200

    overlays/local/pkgs: fix pulseview build
    Seems like we need to add qt5.qttools to nativeBuildInputs
    now? (Actually even before the latest bump of pulseview, so might've
    rather been something else that changed in nixpkgs)

--- overlays/local/pkgs/default.nix
+++ overlays/local/pkgs/default.nix
@@ -126,6 +126,8 @@ with super.lib; {
     };
     version = "9999";
     name = "pulseview-9999";
+    nativeBuildInputs = old.nativeBuildInputs ++ [ super.qt5.qttools ];
+
   });
 
   redshift = (super.redshift.override { withGeolocation = false; });
