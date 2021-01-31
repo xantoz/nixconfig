04229f82434fc228c845530c69cb440b3ac0fe46
Anton Kindestam <antonki@kth.se>
2021-01-31 10:54:17 +0100

    home/modules/services/easystroke: Indentation fix

--- home/modules/services/easystroke.nix
+++ home/modules/services/easystroke.nix
@@ -11,20 +11,20 @@ with lib;
 
   config = mkIf config.services.easystroke.enable {
     systemd.user.services.easystroke = {
-        Unit = {
-          Description = "Easystroke gesture recognition";
-          After = [ "graphical-session-pre.target" ];
-          PartOf = [ "graphical-session.target" ];
-        };
+      Unit = {
+        Description = "Easystroke gesture recognition";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
 
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
 
-        Service = {
-          ExecStart = "${pkgs.stdenv.shell} -l -c 'exec ${pkgs.easystroke}/bin/easystroke'";
-          Restart = "on-failure";
-        };
+      Service = {
+        ExecStart = "${pkgs.stdenv.shell} -l -c 'exec ${pkgs.easystroke}/bin/easystroke'";
+        Restart = "on-failure";
+      };
     };
   };
 }
