cdbefc554695e72514705fc253080669f817f2d0
Anton Kindestam <antonki@kth.se>
2021-02-10 17:27:13 +0100

    Use a ffmpeg with v4l2-request for mpv

--- overlays/local/pkgs/default.nix
+++ overlays/local/pkgs/default.nix
@@ -35,6 +35,17 @@ with super.lib; {
     toolkit = "no";
   };
 
+  ffmpeg-v4l2-request = super.ffmpeg.overrideAttrs(old: {
+    src = super.fetchFromGitHub {
+      owner = "Kwiboo";
+      repo = "FFmpeg";
+      rev = "9236ad7c3e4048f18c4cb79eafbe2a89085de381";
+      sha256 = "0lv1cvh6fnwqbxnxpkpsmny4q24hhbqykj22q0npxnzxh165882m";
+    };
+    buildInputs = old.buildInputs ++ [ super.libudev super.libv4l ];
+    configureFlags = old.configureFlags ++ [ "--enable-libudev" "--enable-libv4l2" "--enable-v4l2-request" ];
+  });
+
   mpv-unwrapped =
     let
       mpv_rev = "c1c0da34eb7536682f5f452f678a0d30fc8a1fec";
@@ -43,6 +54,7 @@ with super.lib; {
         echo "${mpv_rev}"
       '';
     in (super.mpv-unwrapped.override {
+      ffmpeg = self.ffmpeg-v4l2-request;
       openalSupport = true;
       archiveSupport = true;
       vdpauSupport = false;
