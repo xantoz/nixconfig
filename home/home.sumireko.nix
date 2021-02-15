044f1af39f3297d1a3a178c419935025ea1a4ba7
Anton Kindestam <antonki@kth.se>
2021-02-15 10:07:22 +0100

    home.sumireko.nix: Fix xinput settings for touchpad in .xsession
    There is probably a much nicer way of doing this, like actually using
    xorg.conf and such.

--- home/home.sumireko.nix
+++ home/home.sumireko.nix
@@ -65,8 +65,8 @@
     ".xsession".source = pkgs.writeText "dotxinitrc" ''
       xrdb -merge ~/.Xresources
 
-      xinput set-prop 'PS/2 Generic Mouse' 'libinput Middle Emulation Enabled' 1
-      xinput set-prop 'PS/2 Generic Mouse' 'Coordinate Transformation Matrix' 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.300000
+      xinput set-prop 'HAILUCK CO.,LTD USB KEYBOARD Touchpad' 'Coordinate Transformation Matrix' 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.4
+      xinput set-prop 'HAILUCK CO.,LTD USB KEYBOARD Touchpad' 'libinput Tapping Enabled' 1
 
       feh --bg-fill ~/Pictures/Wallpapers/Touhou/Du0glW6V4AA2fdz.jpg &
 
