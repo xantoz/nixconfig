b314f1cf358d032ca5beaf51c573b89466c5683e
Anton Kindestam <antonki@kth.se>
2021-03-01 09:06:51 +0100

    home/emacs: hack to make image-dired nixier
    image-dired expects to find things in PATH, but we instead feed it
    with direct nix-style paths to the programs so we don't neccessarily
    have to include all these programs in the default environment.

--- home/emacs.nix
+++ home/emacs.nix
@@ -35,8 +35,20 @@
     # TODO: how to deal with ~/.emacs-custom ?
     ".config/emacs.d".source = ./config/emacs;
     # ".config/emacs".source = ./config/emacs;
-    ".emacs".source = pkgs.writeText "dotemacs" ''
-      (load "~/.config/emacs.d/init.el")
-    '';
+    ".emacs".source = pkgs.writeText "dotemacs"
+      ''
+        (load "~/.config/emacs.d/init.el")
+
+        ;; fix image-dired on NixOS
+        (setq image-dired-cmd-create-thumbnail-program   "${pkgs.imagemagick}/bin/convert"
+              image-dired-cmd-create-temp-image-program  "${pkgs.imagemagick}/bin/convert"
+              image-dired-cmd-rotate-thumbnail-program   "${pkgs.imagemagick}/bin/mogrify"
+              image-dired-cmd-rotate-original-program    "${pkgs.libjpeg_turbo}/bin/jpegtran"
+              image-dired-cmd-pngnq-program              "${pkgs.pngnq}/bin/pngnq"
+              image-dired-cmd-pngcrush-program           "${pkgs.pngcrush}/bin/pngcrush"
+              image-dired-cmd-optipng-program            "${pkgs.optipng}/bin/optipng"
+              image-dired-cmd-write-exif-data-program    "${pkgs.exiftool}/bin/exiftool"
+              image-dired-cmd-read-exif-data-program     "${pkgs.exiftool}/bin/exiftool")
+     '';
   };
 }
