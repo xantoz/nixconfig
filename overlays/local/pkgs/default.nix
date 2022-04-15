7876d2e09ed8b912e6aadddda647ca0c80df8592
Anton Kindestam <antonki@kth.se>
2022-04-15 12:25:29 +0200

    overlays/local/pkgs: comment out customization of xterm
    For some reason this causes rebuilds of firefox??!!?
    Why?
    
    Maybe I should try to upstream this change so I don't have to build
    firefox myself...

--- overlays/local/pkgs/default.nix
+++ overlays/local/pkgs/default.nix
@@ -1,9 +1,9 @@
 self: super:
 
 with super.lib; {
-  xterm = super.xterm.overrideAttrs(old: {
-    configureFlags = old.configureFlags ++ [ "--enable-exec-xterm" ];
-  });
+  # xterm = super.xterm.overrideAttrs(old: {
+  #   configureFlags = old.configureFlags ++ [ "--enable-exec-xterm" ];
+  # });
 
   m17n_db = super.m17n_db.overrideAttrs(old: {
     patches = [ ../../../patches/m17n_db/0001-add-sv-qwerty.mim.patch ];
