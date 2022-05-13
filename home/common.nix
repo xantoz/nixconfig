457f339530b53c7b896aa4b8d4f94cf43b088026
Anton Kindestam <antonki@kth.se>
2022-05-13 15:17:02 +0200

    home/common: adjust to the new programs.bash module
    instead of just placing the .bashrc file manually..

--- home/common.nix
+++ home/common.nix
@@ -5,38 +5,36 @@
 
   imports = import ./modules/module-list.nix;
 
-  home.file = {
-    ".zile".source = builtins.path { name = "dotzile"; path = ./config/dotfiles/src/.zile; };
-    ".config/youtube-dl/config".source = ./config/dotfiles/src/youtube-dl/config;
-    ".config/feh/themes".source = ./config/dotfiles/src/feh/themes;
-    ".config/redshift.conf".source = ./config/dotfiles/src/redshift.conf;
+  programs.bash = {
+    shellOptions = [
+      "histappend" # Append to history file rather than replacing it.
+      "cmdhist" # Save multiline commands as one entry
 
-    ".config/alacritty".source = ./config/dotfiles/src/alacritty;
-    ".config/kitty".source = ./config/dotfiles/src/kitty;
+      # Below are ones that I didn't use to use, but that were in the
+      # defaults in the bash home-manager module
 
-    ".config/mako".source = ./config/dotfiles/src/mako;
+      # check the window size after each command and, if
+      # necessary, update the values of LINES and COLUMNS.
+      "checkwinsize"
 
-    ".config/waybar".source = ./config/dotfiles/src/waybar;
+      # Extended globbing.
+      "extglob"
+      "globstar"
 
-    ".config/pqivrc".source = pkgs.writeText "pqiv-config" ''
-      [options]
-      fullscreen=1
-      sort=1
-    '';
-
-    ".config/nixpkgs/config.nix".source = pkgs.writeText "unfree_for_nix-shell" ''
-      { allowUnfree = true; }
-    '';
-
-    ".bashrc".source = pkgs.writeText "dotbashrc" ''
+      # Warn if closing shell with running jobs.
+      "checkjobs"
+    ];
+    # TODO: HISTTIMEFORMAT should maybe be added to the module itself?
+    # TODO: historyControl in the bash module does not support the ignoreboth option!
+    # TODO: should prompt command also be added to the module?
+    bashrcExtra = ''
       export HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S - '
-      shopt -s histappend
-      shopt -s cmdhist
-      HISTSIZE=600000
-      HISTFILESIZE=600000
       PROMPT_COMMAND='history -a;history -n'
       export HISTCONTROL=ignoreboth
-
+    '';
+    historySize = 600000;
+    historyFileSize = 600000;
+    initExtra = ''
       alias dush='du -sh -- * | sort -h'
 
       0x0()
@@ -64,6 +62,30 @@
       export PATH="$HOME/.local/bin:$PATH"
 
       export EDITOR='emacsclient -t'
+    '';
+  };
+
+  home.file = {
+    ".zile".source = builtins.path { name = "dotzile"; path = ./config/dotfiles/src/.zile; };
+    ".config/youtube-dl/config".source = ./config/dotfiles/src/youtube-dl/config;
+    ".config/feh/themes".source = ./config/dotfiles/src/feh/themes;
+    ".config/redshift.conf".source = ./config/dotfiles/src/redshift.conf;
+
+    ".config/alacritty".source = ./config/dotfiles/src/alacritty;
+    ".config/kitty".source = ./config/dotfiles/src/kitty;
+
+    ".config/mako".source = ./config/dotfiles/src/mako;
+
+    ".config/waybar".source = ./config/dotfiles/src/waybar;
+
+    ".config/pqivrc".source = pkgs.writeText "pqiv-config" ''
+      [options]
+      fullscreen=1
+      sort=1
+    '';
+
+    ".config/nixpkgs/config.nix".source = pkgs.writeText "unfree_for_nix-shell" ''
+      { allowUnfree = true; }
     '';
   };
 }
