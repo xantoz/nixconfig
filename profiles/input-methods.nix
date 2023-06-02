0443f3680495456d501ed84d12722b383489cef7
xantoz <bountyjedi@gmail.com>
2023-06-02 14:42:41 +0200

    profiles/input-methods: fcitx -> fcitx5
    old fcitx has since been removed. Let's try fcitx5. Although maybe I
    should just roll with ibus since that is what the major DEs seem to
    integrate best with? (I'm also trying to run a major DE on zeke right
    now for some reason or the other)

--- profiles/input-methods.nix
+++ profiles/input-methods.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  # TODO: fcitx is not the best in wayland in particular, replace fcitx usage with dbus
-  i18n.inputMethod.enabled = "fcitx";
-  i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ mozc m17n ];
+  # TODO: consider going with more widely supported ibus instead? although at least we are at fcitx5 now, which should work better in wayland?
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+      fcitx5-m17n
+    ];
+  };
 
   services.xserver.layout = "se(us)";
   services.xserver.xkbOptions = "ctrl:nocaps";
