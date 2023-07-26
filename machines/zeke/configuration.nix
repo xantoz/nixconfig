7d589728c881a074dedd3d37a3b5017bef3b2566
xantoz <bountyjedi@gmail.com>
2023-07-26 11:39:26 +0200

    machines/zeke: Stash away commented out manual nvidia-offload script
    Just saving this here for future reference.
    
    The nvidia-offload script that comes with nixos seems good enough
    though, so I don't really need it I think.

--- machines/zeke/configuration.nix
+++ machines/zeke/configuration.nix
@@ -102,8 +102,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    # (pkgs.writeShellScriptBin "nvidia-offload" ''
+    #   export __NV_PRIME_RENDER_OFFLOAD=1
+    #   export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    #   export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    #   export __VK_LAYER_NV_optimus=NVIDIA_only
+    #   exec "$@"
+    # '')
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
