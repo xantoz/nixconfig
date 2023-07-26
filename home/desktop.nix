2f1ab81f171f3fb06e9c3cfc9b9c84895e527100
xantoz <bountyjedi@gmail.com>
2023-07-26 13:35:01 +0200

    home/desktop.nix: stash away experiment in adding fcitx5 with home-manager
    Not sure if this makes sense to do, adding it in the normal nixpkgs
    way seems to work just as well with autostarting it and everything due
    to xdg-autostart shenanigans (at least when running a full-blown DE
    like I am testing out right now. Might be nice to configure up my
    lightweight alternatives to work similarly though? Or maybe not
    xdg-autostart stuff is sometimes annoying)

--- home/desktop.nix
+++ home/desktop.nix
@@ -160,4 +160,13 @@
   home.file = {
    ".webmacs/init".source = ./config/webmacs;
   };
+
+  # i18n.inputMethod = {
+  #   enabled = "fcitx5";
+  #   fcitx5.addons = with pkgs; [
+  #     fcitx5-mozc
+  #     fcitx5-gtk
+  #     fcitx5-m17n
+  #   ];
+  # };
 }
