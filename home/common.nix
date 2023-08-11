472c40133b8b60544a3c9cd982807876cddef706
Anton Kindestam <antonki@kth.se>
2023-08-11 13:01:33 +0200

    home/common: Fix quoting issue in previous commit
    Maybe test things before you push them?

--- home/common.nix
+++ home/common.nix
@@ -70,20 +70,20 @@
 
       # Output OSC7 so that foot knows which directory we are in
       osc7_cwd() {
-          local strlen=${#PWD}
+          local strlen=''${#PWD}
           local encoded=""
           local pos c o
           for (( pos=0; pos<strlen; pos++ )); do
-              c=${PWD:$pos:1}
-              case "$c" in
-                  [-/:_.!\'\(\)~[:alnum:]] ) o="${c}" ;;
-                  * ) printf -v o '%%%02X' "'${c}" ;;
+              c=''${PWD:$pos:1}
+              case "''$c" in
+                  [-/:_.!\'\(\)~[:alnum:]] ) o="''${c}" ;;
+                  * ) printf -v o '%%%02X' "''${c}" ;;
               esac
-              encoded+="${o}"
+              encoded+="''${o}"
           done
-          printf '\e]7;file://%s%s\e\\' "${HOSTNAME}" "${encoded}"
+          printf '\e]7;file://%s%s\e\\' "''${HOSTNAME}" "''${encoded}"
       }
-      PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }osc7_cwd
+      PROMPT_COMMAND=''${PROMPT_COMMAND:+$PROMPT_COMMAND; }osc7_cwd
     '';
   };
 
