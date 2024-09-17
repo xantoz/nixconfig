27f06d5c52c12823a9dd181eec63c0074ac78240
xantoz <bountyjedi@gmail.com>
2024-09-17 09:48:58 +0200

    zeke: Remove commented-out environment.systemPackages
    We aren't adding any systemPackages in this config here anyway (all
    packages added at the root config level are added only for my user)

--- machines/zeke/configuration.nix
+++ machines/zeke/configuration.nix
@@ -115,19 +115,6 @@
   # CUDA support in Blender and more (See: https://discourse.nixos.org/t/how-to-get-cuda-working-in-blender/5918/12)
   nixpkgs.config.cudaSupport = true;
 
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    # (pkgs.writeShellScriptBin "nvidia-offload" ''
-    #   export __NV_PRIME_RENDER_OFFLOAD=1
-    #   export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    #   export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    #   export __VK_LAYER_NV_optimus=NVIDIA_only
-    #   exec "$@"
-    # '')
-  ];
-
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
