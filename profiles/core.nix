e2b84de7a9a414ed3b2ce8f48a8cf46fe741985a
xantoz <bountyjedi@gmail.com>
2024-12-25 17:49:10 +0100

    blargh. flakes suck

--- profiles/core.nix
+++ profiles/core.nix
@@ -73,8 +73,8 @@
 
   # programs.simpleserver.enable = true;
 
-  programs.screen.screenrc = builtins.readFile ../home/config/dotfiles/src/.screenrc;
-  programs.screen.enable = true;
+  # programs.screen.screenrc = builtins.readFile ../home/config/dotfiles/src/.screenrc;
+  # programs.screen.enable = true;
 
   services.gpm.enable = true;
 
