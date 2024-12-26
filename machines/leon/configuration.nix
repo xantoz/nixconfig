b2bd120943f50c0420b312e53d66b0194a9a8d4c
Anton Kindestam <antonki@kth.se>
2024-12-26 13:20:49 +0100

    leon: programs.steam.enable = true

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -159,6 +159,8 @@
     htop
   ];
 
+  programs.steam.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
