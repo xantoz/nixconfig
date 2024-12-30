12412f440d39c7cc6cc51c78a0e36097a060d640
Anton Kindestam <antonki@kth.se>
2024-12-30 11:32:00 +0100

    machines/leon: whitespace cleanup

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -225,6 +225,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
-
