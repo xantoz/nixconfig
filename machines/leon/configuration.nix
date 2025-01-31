1918713c65ed9d256834cf8d8b8e9d6768c7539e
Anton Kindestam <antonki@kth.se>
2025-01-31 15:39:58 +0100

    machines/leon: Tidy up the hardware.nvidia section a little

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -195,15 +195,16 @@
   services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.modesetting.enable = true;   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-  hardware.nvidia.open = false; # should be fine with the open kernel module because we are Mobile RTX 3070 => Ampere. Testing non-open though, since I had trouble
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
-    amdgpuBusId = "PCI:34:0:0";
-    nvidiaBusId = "PCI:1:0:0";
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    modesetting.enable = true;   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+    open = false; # should be fine with the open kernel module because we are Mobile RTX 3070 => Ampere. Testing non-open though, since I had trouble
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
+      amdgpuBusId = "PCI:34:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   programs.firefox.enable = true;
