81c8d99087d14485a5348368487680816d238ddf
Anton Kindestam <antonki@kth.se>
2025-02-01 11:41:57 +0100

    machines/leon: Enable powerManagement for the nvidia GPU
    hopefully fixes bork graphics on suspend?

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -200,6 +200,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.latest;   # Optionally, you may need to select the appropriate driver version for your specific GPU.
     modesetting.enable = true;   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
     open = true; # should be fine with the open kernel module because we are Mobile RTX 3070 => Ampere. Testing non-open though, since I had trouble
+    powerManagement.enable = true; # Should help with graphics going derp on suspend?
     # prime = {
     #   offload.enable = true;
     #   offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
