853909054c67b8243ff005ac05d81d38f4d127b7
Anton Kindestam <antonki@kth.se>
2025-02-02 11:45:02 +0100

    machines/leon: Add the amdgpu cap_sys_nice_begone patch
    This makes it so we can get high priority queues for AMDGPU without
    CAP_SYS_NICE, which is needed for SteamVR, since NixOS bwrap
    shenanigans.
    
    TODO: I need to fashion a patch that does this for the nvidia-open
    kernel module as well.

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -81,6 +81,14 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelPatches = [
     {
+      name = "amdgpu-ignore-ctx-privileges";
+      patch = pkgs.fetchpatch {
+        name = "cap_sys_nice_begone.patch";
+        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+      };
+    }
+    {
       name = "preempt_rt";
       patch = null;
       extraConfig = ''
