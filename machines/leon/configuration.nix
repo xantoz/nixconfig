165b7e9789be4c0fb2717c1447a3e2dfb458903e
xantoz <bountyjedi@gmail.com>
2025-02-05 20:50:40 +0100

    machines/leon: Disable the attempt at patching nvidia-open kernel driver for now
    I can't quite seem to get it to work...

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -235,13 +235,14 @@
     # Hopefully this is 570.
     # We patch the open kernel module to behave like CAP_SYS_NICE is always set for the benefit of SteamVR (since NixOS bwraps it and stuff)
     # FIXME: I'm not too sure that this actually applies the overriden package where we want it
-    package =
-      let
-        nvidiaPkg = config.boot.kernelPackages.nvidiaPackages.latest;
-      in nvidiaPkg.overrideAttrs (old: {
-        patches = old.patches ++ [ ./0001-behave-like-cap_sys_nice-is-always-set.patch ];
-        # patchesOpen = old.patchesOpen ++ [ ./0001-behave-like-cap_sys_nice-is-always-set.patch ];
-      });
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    # package =
+    #   let
+    #     nvidiaPkg = config.boot.kernelPackages.nvidiaPackages.beta;
+    #   in nvidiaPkg.overrideAttrs (old: {
+    #     patches = old.patches ++ [ ./0001-behave-like-cap_sys_nice-is-always-set.patch ];
+    #     # patchesOpen = old.patchesOpen ++ [ ./0001-behave-like-cap_sys_nice-is-always-set.patch ];
+    #   });
     modesetting.enable = true;   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
     gsp.enable = true;           # We have a GSP-enabled nvidia GPU (I think this is implied by open = true, but probably best to put this here too)
     open = true; # should be fine with the open kernel module because we are Mobile RTX 3070 => Ampere. Testing non-open though, since I had trouble
@@ -253,12 +254,12 @@
     #   nvidiaBusId = "PCI:1:0:0";
     # };
   };
-  # Ugly-hack the extraModules setting because the override above doesn't quite work
-  boot.extraModulePackages = lib.mkForce [
-    (config.boot.kernelPackages.nvidiaPackages.latest.open.overrideAttrs(old: {
-      patches = old.patches ++ [ ./0001-behave-like-cap_sys_nice-is-always-set.patch ];
-    }))
-  ];
+  # # Ugly-hack the extraModules setting because the override above doesn't quite work
+  # boot.extraModulePackages = lib.mkForce [
+  #   (config.boot.kernelPackages.nvidiaPackages.beta.open.overrideAttrs(old: {
+  #     patches = old.patches ++ [ ./0001-behave-like-cap_sys_nice-is-always-set.patch ];
+  #   }))
+  # ];
 
 
   programs.firefox.enable = true;
