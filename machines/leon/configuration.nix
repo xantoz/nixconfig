86c45cae2bcfd1fb44edbf81d288f9ddac4379e4
xantoz <bountyjedi@gmail.com>
2025-02-16 23:32:12 +0100

    machines/leon: Reset GPU config to be good for a hybrid scenario
    still works for wayland sessions in the discrete graphics setting,
    but X11 doesn't like to start with this conf on...
    
    TODO: Figure out some way to support both hybrid graphics setting in
    bios and discrete graphics only setting in BIOS (perhaps even a mode
    that completely disables the AMD?)
    
    In fact I kinda wish there was a way to build several different
    profiles all at once, and I just choose at boot or something.
    Maybe flakes?

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -294,7 +294,7 @@
 
 
   services.xserver.videoDrivers = [
-#    "amdgpu"
+   "amdgpu"
     "nvidia"
   ];
 
@@ -307,12 +307,12 @@
     #disableOthers = true;
     gspMode = "yes-with-open-driver";
     disableOthers = false;
-#    prime = {
-#      offload.enable = true;
-#      offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
-#      amdgpuBusId = "PCI:34:0:0";
-#      nvidiaBusId = "PCI:1:0:0";
-#    };
+   prime = {
+     offload.enable = true;
+     offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
+     amdgpuBusId = "PCI:34:0:0";
+     nvidiaBusId = "PCI:1:0:0";
+   };
   };
 
   # Open ports in the firewall.
