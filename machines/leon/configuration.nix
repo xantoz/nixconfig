d63d36c7864e4a6c880f3cfb05991cf8c40c7d6d
xantoz <bountyjedi@gmail.com>
2025-03-02 10:28:22 +0100

    get rid of the gs.sh thing

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -162,36 +162,6 @@
 
       obs-studio
       mcomix
-
-      (writeShellScriptBin "gs.sh" ''
-        #!/usr/bin/env bash
-        set -xeuo pipefail
-
-        gamescopeArgs=(
-            --adaptive-sync # VRR support
-            --hdr-enabled
-            --mangoapp # performance overlay
-            --rt
-            --steam
-        )
-        steamArgs=(
-            -pipewire-dmabuf
-            -tenfoot
-        )
-        mangoConfig=(
-            cpu_temp
-            gpu_temp
-            ram
-            vram
-        )
-        mangoVars=(
-            MANGOHUD=1
-            MANGOHUD_CONFIG="''\$(IFS=,; echo "''\${mangoConfig[*]}")"
-        )
-
-        export "''\${mangoVars[@]}"
-        exec gamescope "''\${gamescopeArgs[@]}" -- steam "''\${steamArgs[@]}"
-      '')
     ];
   };
 
