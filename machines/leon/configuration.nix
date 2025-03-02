c64c539381d673e988e5eb3414b5fde62c55fb82
xantoz <bountyjedi@gmail.com>
2025-03-02 10:29:45 +0100

    leon: disable the amdgpu ignore privileges patch for now
    to avoid having to rebuild the kernel

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -87,16 +87,16 @@
   programs.gamemode.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPatches = [
-    {
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }
-  ];
+  # boot.kernelPatches = [
+  #   {
+  #     name = "amdgpu-ignore-ctx-privileges";
+  #     patch = pkgs.fetchpatch {
+  #       name = "cap_sys_nice_begone.patch";
+  #       url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+  #       hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+  #     };
+  #   }
+  # ];
 
   boot.binfmt.emulatedSystems = [
     "aarch64-linux"
