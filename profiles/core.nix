585f6bf91d3043164c7cfd84b5299ff6a5f0db72
xantoz <bountyjedi@gmail.com>
2025-03-14 08:24:57 +0100

    Revert "Use an overlays-compat thing from the NixOS wiki to make sure NIX_PATH and nixpkgs.overlays always point at the same overlays"
    This reverts commit 6261bc2c0960038f7b3fe7fecedbad40f7291e79.
    
    Turns out that this overlays-compat thing, while seemingly working ok
    for my own local overlay, ends up in some sort of disastrous explosion
    when I have the nixpkgs-xr overlay.
    
    Although it seems like that has actually been breaking my builds ever
    since I made it a submodule though for some reason

--- profiles/core.nix
+++ profiles/core.nix
@@ -19,7 +19,7 @@ in {
     nixPath = [
       "/etc/nixos"
       "nixpkgs=/etc/nixos/nixpkgs"
-      "nixpkgs-overlays=/etc/nixos/overlays-compat"
+      "nixpkgs-overlays=/etc/nixos/overlays/local"
       "nixos-config=/etc/nixos/configuration.nix"
     ];
     settings = {
