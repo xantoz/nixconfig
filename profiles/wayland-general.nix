4ae3b4605deec0f27f1a4005b3c025622f2e6071
xantoz <bountyjedi@gmail.com>
2025-04-13 10:51:46 +0200

    profiles/wayland-general: Add wl-clipboard utils
    Also some other interesting tools that might be of use, but currently
    commented out

--- profiles/wayland-general.nix
+++ profiles/wayland-general.nix
@@ -8,5 +8,11 @@
     # Usable minimal wayland-native terminals
     foot
     #stupidterm
+
+    wl-clipboard                # Clipboard management and stuff
+    # wl-clip-persist
+    # cliphist                  # more fancy clipboard manager. home-manager module exists
+    # clipman                   # another more fancy clipboard manager. home-manager module exists
+    # clipmon                   # supports persisting after app exists and supports more than text?
   ];
 }
