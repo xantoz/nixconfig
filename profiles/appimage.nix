db6f59bc64dc5488b8103a2bf1252a22d5c50e51
xantoz <bountyjedi@gmail.com>
2025-07-19 17:06:29 +0200

    profiles/appimage: Add one more extraPkgs neede for VRCFT appimage

--- profiles/appimage.nix
+++ profiles/appimage.nix
@@ -7,6 +7,7 @@
     package = pkgs.appimage-run.override {
       extraPkgs = pkgs: [
         pkgs.dotnet-runtime     # Needed for VRCX for one
+        pkgs.icu                # Needed by VRCFaceTracking.Avalonia appimage
       ];
     };
   };
