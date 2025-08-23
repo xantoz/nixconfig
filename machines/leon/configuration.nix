96135dbbbd543577103b39d806d02d2da04083ba
xantoz <bountyjedi@gmail.com>
2025-08-23 15:10:27 +0200

    machines/leon: add ddrescueview to environment.systemPackages

--- machines/leon/configuration.nix
+++ machines/leon/configuration.nix
@@ -184,6 +184,9 @@
     #dosbox
     dosbox-x
     wine
+
+    ddrescueview
+
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     zile
     wget
