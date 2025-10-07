fcaf4f5c6a90e6e2ecb1ae3ea4d77de9909fea59
xantoz <bountyjedi@gmail.com>
2025-10-07 15:08:51 +0200

    profiles/core: Comment out sv_SE/IBM437 locale because it won't build
    this doesn't build for some strange reason.... at least I can mix and
    match and just use LC_CTYPE=en_US.IBM437 as a workaround.

--- profiles/core.nix
+++ profiles/core.nix
@@ -102,7 +102,9 @@
     defaultLocale = "en_US.UTF-8";
     extraLocales = [
       "en_US/ISO-8859-1"
+      "en_US/IBM437"
       "sv_SE/ISO-8859-1"
+      # "sv_SE/IBM437"
       "sv_SE.UTF-8/UTF-8"
       "ja_JP.EUC-JP/EUC-JP"
       "ja_JP.UTF-8/UTF-8"
