1d5ce4f82df4da23c460f267a33e5953f33110b5
xantoz <bountyjedi@gmail.com>
2026-01-02 15:54:50 +0100

    machines/leon/gpu: use proprietary nvidia driver again

--- machines/leon/gpu.nix
+++ machines/leon/gpu.nix
@@ -6,36 +6,36 @@
   ################################################################################
   ## nouveau/NVK settings
   ################################################################################
-  services.xserver.videoDrivers = [
-    "amdgpu"
-    "nouveau"
-  ];
+  # services.xserver.videoDrivers = [
+  #   "amdgpu"
+  #   "nouveau"
+  # ];
 
   # TODO: Perhaps make this into a machine-specific module (still leveraging my xz.nvidia which is a bit more generic)
 
   #################################################################################
   ## settings for amdgpu main with nvidia offload (might happen to work even if mux set to nvidia if you are lucky)
   #################################################################################
-  # services.xserver.videoDrivers = [
-  #  "amdgpu"
-  #   "nvidia"
-  # ];
-  # xz.nvidia = {
-  #   enable = true;
-  #   rmIntrLockingMode = true;
-  #   # gspMode = "no-without-modesetting";
-  #   # disableOthers = true;
-  #   #gspMode = "no-without-simpledrm";
-  #   #disableOthers = true;
-  #   gspMode = "yes-with-open-driver";
-  #   disableOthers = false;
-  #   prime = {
-  #     offload.enable = true;
-  #     offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
-  #     amdgpuBusId = "PCI:34:0:0";
-  #     nvidiaBusId = "PCI:1:0:0";
-  #   };
-  # };
+  services.xserver.videoDrivers = [
+    "amdgpu"
+    "nvidia"
+  ];
+  xz.nvidia = {
+    enable = true;
+    rmIntrLockingMode = true;
+    # gspMode = "no-without-modesetting";
+    # disableOthers = true;
+    #gspMode = "no-without-simpledrm";
+    #disableOthers = true;
+    gspMode = "yes-with-open-driver";
+    disableOthers = false;
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true; # Gives us the nvidia-offload convenience script
+      amdgpuBusId = "PCI:34:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 
   #################################################################################
   ## settings for nvidia only, amdgpu completely disabled
