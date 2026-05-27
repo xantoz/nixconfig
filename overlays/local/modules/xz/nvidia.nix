0ceffe1ac2b22d6f51a3e286d52584000afbd468
xantoz <bountyjedi@gmail.com>
2026-05-27 11:09:48 +0200

    overlays/local/modules/xz/nvidia: switch to new CUDA binary cache

--- overlays/local/modules/xz/nvidia.nix
+++ overlays/local/modules/xz/nvidia.nix
@@ -51,13 +51,11 @@ in {
     in lib.mkIf cfg.enable {
       nixpkgs.config.cudaSupport = true;
 
-      # Add the nix-community cachix. This should hopefully give me binary cache for packages built with cuda enabled, so I don't have to rebuild blender all the time
-      nix.settings.substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      nix.settings.trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
+      # Add the CUDA binary cache. This should hopefully give me binary cache for packages built with cuda enabled, so I don't have to rebuild blender all the time
+      nix.settings = {
+        substituters = [ "https://cache.nixos-cuda.org" ];
+        trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+      };
 
       boot.kernelParams =
         lib.optional (!gspEnabled) "NVreg_EnableGpuFirmware=0"
