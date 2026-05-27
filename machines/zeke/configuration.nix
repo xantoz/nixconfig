0db4994d2df1e96c8311fff3f249d00a672cb978
xantoz <bountyjedi@gmail.com>
2026-05-27 13:01:56 +0200

    zeke: switch away from ibus input methods and to fcitx
    ibus is borked on xwayland windows at least in KDE plasma wayland

--- machines/zeke/configuration.nix
+++ machines/zeke/configuration.nix
@@ -12,7 +12,7 @@
       ./hardware-configuration.nix
       ../../profiles/core.nix
       ../../profiles/graphical-kde.nix
-      ../../profiles/input-methods-ibus.nix
+      ../../profiles/input-methods.nix
       ../../profiles/bluetooth.nix
       ../../profiles/laptop.nix
       # ../../profiles/sway.nix
